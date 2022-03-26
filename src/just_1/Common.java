7b48142e86aba6707c436c4cf74e3a52e1fb8d80
Ram Bhosale <bhosaleram41@gmail.com>
2022-03-26 15:36:31 +0530

    file

---
+++ src/just_1/Common.java
@@ -0,0 +1,9 @@
+package just_1;
+
+public class Common {
+ 
+	  int a=10;
+      int b=20;
+      
+
+}
---
+++ src/just_1/Room.java
@@ -0,0 +1,13 @@
+package just_1;
+
+public class Room {
+
+	public static void main(String[] args) {
+
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+
+	}
+
+}
